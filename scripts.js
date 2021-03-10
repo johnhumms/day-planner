724495b0b2ea44e210c6c721fdeb9824a9b14fc2
johnhumms <johnehummeljr@gmail.com>
2021-03-09 22:07:23 -0600

    added 10 and 11 time slots

--- scripts.js
+++ scripts.js
@@ -1,3 +1,58 @@
 //Moment JS for time at top
 var time = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 $("#currentDay").text(time);
+
+//9 am
+const nineBtn = document.getElementById('button') //make button visible
+const nineSave = document.getElementById('nine-save') 
+
+nineBtn.addEventListener('click', () => {
+    let nineTask = document.getElementById('nine-input').value; //.value targets value inside the box
+    localStorage.setItem('nine-am-task', nineTask )
+
+    renderTaskNine();
+    })
+
+    function renderTaskNine () {
+        let nineData = localStorage.getItem('nine-am-task')
+        nineSave.textContent = nineData
+}
+
+renderTaskNine();
+
+
+//10am
+const tenBtn = document.getElementById('ten-button')
+const tenSave = document.getElementById('ten-save') 
+
+tenBtn.addEventListener('click', () => {
+    let tenTask = document.getElementById('ten-input').value;
+    localStorage.setItem('ten-am-task', tenTask )
+
+    renderTaskTen();
+})
+
+function renderTaskTen () {
+    let tenData = localStorage.getItem('ten-am-task')
+    tenSave.textContent = tenData
+}
+
+renderTaskTen();
+
+//11am
+const elevenBtn = document.getElementById('eleven-button')
+const elevenSave = document.getElementById('eleven-save') 
+
+elevenBtn.addEventListener('click', () => {
+    let elevenTask = document.getElementById('eleven-input').value;
+    localStorage.setItem('eleven-am-task', elevenTask )
+
+    renderTaskEleven();
+})
+
+function renderTaskEleven () {
+    let elevenData = localStorage.getItem('eleven-am-task')
+    elevenSave.textContent = elevenData
+}
+
+renderTaskEleven();
